52db38175f0199998009d537df0b16ce5ccd1c17
Polina Zakharova <polina.zakharova.pn.2024@lpnu.ua>
2025-10-14 21:25:40 +0300

    apdate first commit

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const concat = require('gulp-concat');
 const cssnano = require('gulp-cssnano');
 const rename = require('gulp-rename');
 const uglify = require('gulp-uglify');
-const imagemin = require('gulp-imagemin');
+// const imagemin = require('gulp-imagemin');
 const sass = require('gulp-sass')(require('sass'));
 
 const paths = {
@@ -41,11 +41,11 @@ function js_task() {
     .pipe(browserSync.stream());
 }
 
-function img_task() {
-  return src(paths.img.src)
-    .pipe(imagemin())
-    .pipe(dest(paths.img.dest));
-}
+// function img_task() {
+//   return src(paths.img.src)
+//     .pipe(imagemin())
+//     .pipe(dest(paths.img.dest));
+// }
 
 function serve() {
   browserSync.init({
@@ -57,15 +57,17 @@ function serve() {
   watch(paths.html.src, html_task);
   watch(paths.scss.src, scss_task);
   watch(paths.js.src,   js_task);
-  watch(paths.img.src,  img_task).on('change', browserSync.reload);
+  // watch(paths.img.src,  img_task).on('change', browserSync.reload);
 }
 
-const build = parallel(html_task, scss_task, js_task, img_task);
+// const build = parallel(html_task, scss_task, js_task, img_task);
+
+const build = parallel(html_task, scss_task, js_task);
 
 exports.html_task = html_task;
 exports.scss_task = scss_task;
 exports.js_task   = js_task;
-exports.img_task  = img_task;
+// exports.img_task  = img_task;
 exports.build     = build;
 exports.default   = series(build, serve);
 
