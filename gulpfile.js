cf12c629583c333065924b54415ead0c1235cc70
Polina Zakharova <polina.zakharova.pn.2024@lpnu.ua>
2025-10-29 15:57:40 +0200

    last commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,73 +1,79 @@
-const { src, dest, series, parallel, watch } = require('gulp');
+const {src, dest, series, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
 const concat = require('gulp-concat');
 const cssnano = require('gulp-cssnano');
 const rename = require('gulp-rename');
 const uglify = require('gulp-uglify');
-// const imagemin = require('gulp-imagemin');
+const imagemin = require('gulp-imagemin');
 const sass = require('gulp-sass')(require('sass'));
 
 const paths = {
-  html:  { src: 'src/**/*.html',            dest: 'dist/' },
-  scss:  { src: 'src/scss/**/*.scss', entry: 'src/scss/main.scss', dest: 'dist/css/' },
-  js:    { src: 'src/js/**/*.js',           dest: 'dist/js/' },
-  img:   { src: 'src/img/**/*.{png,jpg,jpeg,svg,gif,webp}', dest: 'dist/img/' }
+    html: {
+        src: 'app/**/*.html',
+        dest: 'dist'
+    },
+    scss: {
+        src: 'app/scss/**/*.scss',
+        dest: 'dist/css/'
+    },
+    js: {
+        src: 'app/js/**/*.js',
+        dest: 'dist/js/'
+    },
+    img: {
+        src: 'app/img{,s}/**/*.{png,jpg,jpeg,svg,gif,webp}',
+        dest: 'dist/imgs'
+    }
 };
 
 function html_task() {
-  return src(paths.html.src)
-    .pipe(dest(paths.html.dest))
-    .pipe(browserSync.stream());
+    return src(paths.html.src)
+        .pipe(dest(paths.html.dest));
 }
 
 function scss_task() {
-  return src(paths.scss.entry)
-    .pipe(sass().on('error', sass.logError))
-    .pipe(concat('main.css'))
-    .pipe(dest(paths.scss.dest))
-    .pipe(cssnano())
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(dest(paths.scss.dest))
-    .pipe(browserSync.stream());
+    return src(paths.scss.src)
+        .pipe(concat('main.scss'))
+        .pipe(sass().on('error', sass.logError))
+        .pipe(cssnano())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(dest(paths.scss.dest));
 }
 
 function js_task() {
-  return src(paths.js.src)
-    .pipe(concat('main.js'))
-    .pipe(dest(paths.js.dest))
-    .pipe(uglify())
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(dest(paths.js.dest))
-    .pipe(browserSync.stream());
+    return src(paths.js.src)
+        .pipe(concat('main.js'))
+        .pipe(dest(paths.js.dest))
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(dest(paths.js.dest));
 }
 
-// function img_task() {
-//   return src(paths.img.src)
-//     .pipe(imagemin())
-//     .pipe(dest(paths.img.dest));
-// }
+function img_task() {
+    return src(paths.img.src)
+        .pipe(imagemin())
+        .pipe(dest(paths.img.dest));
+}
 
 function serve() {
-  browserSync.init({
-    server: { baseDir: 'dist' },
-    notify: false,
-    open: false
-  });
-
-  watch(paths.html.src, html_task);
-  watch(paths.scss.src, scss_task);
-  watch(paths.js.src,   js_task);
-  // watch(paths.img.src,  img_task).on('change', browserSync.reload);
+    browserSync.init({
+        server: {baseDir: 'dist'},
+        notify: false,
+        open: true
+    });
 }
 
-// const build = parallel(html_task, scss_task, js_task, img_task);
+function reload() {
+    browserSync.reload();
+}
 
-const build = parallel(html_task, scss_task, js_task);
+function watch_task() {
+    watch(paths.html.src, series(html_task, reload));
+    watch(paths.scss.src, series(scss_task, reload));
+    watch(paths.js.src, series(js_task, reload));
+    watch(paths.img.src, series(img_task, reload));
+}
 
-exports.html_task = html_task;
-exports.scss_task = scss_task;
-exports.js_task   = js_task;
-// exports.img_task  = img_task;
-exports.build     = build;
-exports.default   = series(build, serve);
+const build = series(html_task, scss_task, js_task, img_task);
 
+exports.default = series(build, serve, watch_task);
